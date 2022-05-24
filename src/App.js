d3e42850480fc2c9602f42ebd650623101444d86
alexisaldrete2018 <40009553+alexisaldrete2018@users.noreply.github.com>
2022-05-23 18:57:37 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+import Catalog from './components/catalog';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <h1>Welcome to Alexis-Mart</h1>
+      <Catalog></Catalog>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
